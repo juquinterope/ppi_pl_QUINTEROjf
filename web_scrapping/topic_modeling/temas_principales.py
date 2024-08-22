5903fa36d13d5f2a176331f07afe52dcd7f15bd0
Juanqu <jfquintero261@gmail.com>
2024-08-22 16:52:48 -0500

    clean files

--- web_scrapping/topic_modeling/temas_principales.py
+++ web_scrapping/topic_modeling/temas_principales.py
@@ -1,3 +1,10 @@
+import nltk # type:ignore
+from nltk.corpus import stopwords # type:ignore
+from nltk.stem import SnowballStemmer # type:ignore
+from sklearn.feature_extraction.text import TfidfVectorizer # type:ignore
+from gensim import corpora, models # type:ignore
+from wordcloud import WordCloud # type:ignore
+import matplotlib.pyplot as plt # type:ignore
 from news_titles import titulos_noticias
 
 
